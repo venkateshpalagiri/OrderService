6ac53b174b9ec2ce7995c00f4d8ccaf0d067cb5c
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2025-01-23 10:48:06 -0500

    Order entity created

--- src/main/java/com/venkatesh/OrderService/entity/Order.java
+++ src/main/java/com/venkatesh/OrderService/entity/Order.java
@@ -5,13 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.Instant;
 
 @Entity
+@Table(name = "ORDER_DETAILS")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Order {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long orderId;
+    @Column(name = "PRODUCT_ID")
+    private long productId;
+    @Column(name = "PRODUCT_QUANTITY")
+    private long productQuantity;
+    @Column(name = "ORDER_DATE")
+    private Instant orderDate;
+    @Column(name = "ORDER_STATUS")
+    private String orderStatus;
+    @Column(name = "ORDER_AMOUNT")
+    private long orderAmount;
 }
