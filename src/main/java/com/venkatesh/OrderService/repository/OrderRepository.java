bfe3f16d113678d3ac0a58db4c213f6cf20c9032
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2025-01-24 22:57:10 -0500

    OrderRepository created

---
+++ src/main/java/com/venkatesh/OrderService/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.venkatesh.OrderService.repository;
+
+import com.venkatesh.OrderService.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
