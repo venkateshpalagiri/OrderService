6fc0f2da16a7ce0796c96c916fe38f5d55f4315b
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2025-02-04 22:28:55 -0500

    removed unused imports

--- src/main/java/com/venkatesh/OrderService/model/OrderRequest.java
+++ src/main/java/com/venkatesh/OrderService/model/OrderRequest.java
@@ -1,11 +1,16 @@
 package com.venkatesh.OrderService.model;
 
-import javax.persistence.Column;
-import java.time.Instant;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OrderRequest {
     private long orderId;
     private long productQuantity;
     private long orderAmount;
-
+    private PaymentMode paymentMode;
 }
