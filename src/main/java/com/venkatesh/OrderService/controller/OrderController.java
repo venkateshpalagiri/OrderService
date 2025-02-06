aead4312ec53cb16283289c79168ca5a9620ab44
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2025-02-06 10:48:06 -0500

    service layer autowired

--- src/main/java/com/venkatesh/OrderService/controller/OrderController.java
+++ src/main/java/com/venkatesh/OrderService/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.venkatesh.OrderService.controller;
 
+import com.venkatesh.OrderService.service.OrderService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 @Log4j2
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
 }
