ee4475a6d3cb8b03bfda48fddd4ad65b42f5021f
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2025-02-07 23:20:11 -0500

    PostMapping started

--- src/main/java/com/venkatesh/OrderService/controller/OrderController.java
+++ src/main/java/com/venkatesh/OrderService/controller/OrderController.java
@@ -1,11 +1,12 @@
 package com.venkatesh.OrderService.controller;
 
+import com.venkatesh.OrderService.model.OrderRequest;
 import com.venkatesh.OrderService.service.OrderService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -13,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+    @PostMapping("/placeOrder")
+    public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest){
+        
+    }
+
 }
