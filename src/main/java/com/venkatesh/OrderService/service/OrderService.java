ffab731127af689648ccd5012ea31b6a583ca4a3
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2025-02-14 11:33:50 -0500

    streak

--- src/main/java/com/venkatesh/OrderService/service/OrderService.java
+++ src/main/java/com/venkatesh/OrderService/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.venkatesh.OrderService.service;
 
+import com.venkatesh.OrderService.entity.Order;
 import com.venkatesh.OrderService.model.OrderRequest;
 
 public interface OrderService {
     long placeOrder(OrderRequest orderRequest);
+
+    Order getOrder(long id);
 }
--- src/main/java/com/venkatesh/OrderService/service/OrderServiceImpl.java
+++ src/main/java/com/venkatesh/OrderService/service/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.venkatesh.OrderService.service;
 
+import com.venkatesh.OrderService.entity.Order;
 import com.venkatesh.OrderService.model.OrderRequest;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +9,10 @@ public class OrderServiceImpl implements OrderService{
     @Override
     public long placeOrder(OrderRequest orderRequest) {
         return 0;
+    }
+
+    @Override
+    public Order getOrder(long id) {
+        return null;
     }
 }
